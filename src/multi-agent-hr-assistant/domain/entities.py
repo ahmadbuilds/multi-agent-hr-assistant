c9b450d58a58d92d6a5ea61bdf0411bb0b58c5c6
Ahmad <crisitiano678@gmail.com>
2026-01-17 13:11:42 +0500

    Structured Output response for the LLM Schema

--- src/multi-agent-hr-assistant/domain/entities.py
+++ src/multi-agent-hr-assistant/domain/entities.py
@@ -1,6 +1,13 @@
 from pydantic import BaseModel,Field
-
+from domain.entities import IntentType
 #pydantic model to represent user query
 class UserQuery(BaseModel):
     query:str=Field(description="query string from the user which needs to be answered")
     isUploaded:bool=Field(description="boolean flag to indicate if the user has uploaded any document or not")
+    isAdmin:bool=Field(description="boolean flag to indicate the Manipulation of Policy Documents")
+
+
+#pydantic model for Supervisor Structured Output
+class Supervisor_structured_output(BaseModel):
+    summary:str=Field(description="summary of the supervisor's analysis of the intent and routing decision")
+    intent:IntentType=Field(description="identified intent from the user query")
