8938b52dea5efc19d79582b37ae044f4be584d0c
Ahmad <crisitiano678@gmail.com>
2026-01-28 21:50:28 +0500

    Ticket creation Entity Created

--- src/multi-agent-hr-assistant/domain/entities.py
+++ src/multi-agent-hr-assistant/domain/entities.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel,Field
 from domain.entities import IntentType
+from domain.intents import TicketType,TicketStatusType
+from typing import Optional
 #pydantic model to represent user query
 class UserQuery(BaseModel):
     query:str=Field(description="query string from the user which needs to be answered")
@@ -11,3 +13,11 @@ class UserQuery(BaseModel):
 class Supervisor_structured_output(BaseModel):
     summary:str=Field(description="summary of the supervisor's analysis of the intent and routing decision")
     intent:IntentType=Field(description="identified intent from the user query")
+
+#pydantic model for Creating Ticket
+class TicketCreation(BaseModel):
+    ticket_type:TicketType=Field(description="type of ticket to be created")
+    subject:str=Field(description="subject of the ticket")
+    description:str=Field(description="detailed description of the ticket")
+    status:TicketStatusType=Field(description="current status of the ticket",default="in_progress")
+    leave_days:Optional[int]=Field(description="number of leave days requested, applicable only for leave tickets",default=None)
