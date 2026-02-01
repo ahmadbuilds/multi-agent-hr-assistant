6176046f9cc9bb5695c06aa74bdf6de75fa88e4b
Ahmad <crisitiano678@gmail.com>
2026-02-01 22:36:38 +0500

    wrapper function for the Supervisor tool

---
+++ src/multi-agent-hr-assistant/domain/tools/supervisor_tool.py
@@ -0,0 +1,20 @@
+from domain.ports import ClerkGraphExecutionPort
+from langchain.tools import tool
+
+#Wrapper function to execute the Clerk Agent State Graph
+def make_supervisor_execute_clerk_graph_tool(executor:ClerkGraphExecutionPort):
+    @tool(
+        name="execute_clerk_graph",
+        description="Use this tool to execute the Clerk Agent State Graph for handling HR related queries."
+    )
+    def execute_clerk_graph()->dict:
+        """
+        Tool function to execute the Clerk Agent State Graph
+        Args:
+            state (ClerkState): Current state of the Clerk Agent
+        Returns:
+            None
+        """
+        #need to implement redis for completion
+        executor.execute_clerk_agent_graph()
+        return {}
