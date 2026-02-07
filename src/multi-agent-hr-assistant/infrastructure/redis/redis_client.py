3089738627b9e0e417dc107f53a16dccc10ff812
Ahmad <crisitiano678@gmail.com>
2026-02-08 00:23:58 +0500

    Added Exception handling in redis client

--- src/multi-agent-hr-assistant/infrastructure/redis/redis_client.py
+++ src/multi-agent-hr-assistant/infrastructure/redis/redis_client.py
@@ -5,23 +5,36 @@ redis=get_redis_client()
 
 #function to save agent state to Redis
 def save_agent_state(agent_state:AgentState):
-    redis.set(
-        agent_state.user_id,
-        f"user_id:{agent_state.user_id}:conversation_id:{agent_state.key}:state",
-        json.dumps(agent_state.state)
-    )
+    try:
+        redis.set(
+            agent_state.user_id,
+            f"user_id:{agent_state.user_id}:conversation_id:{agent_state.key}:state",
+            json.dumps(agent_state.state)
+        )
+    except Exception as e:
+        print("Error saving agent state to Redis:", str(e))
 
 #function to retrieve agent state from Redis
 def get_agent_state(user_id:str, key:str) -> dict:
-    state_json=redis.get(f"user_id:{user_id}:conversation_id:{key}:state")
-    if state_json:
-        return json.loads(state_json)
-    return None
+    try:
+        state_json = redis.get(f"user_id:{user_id}:conversation_id:{key}:state")
+        if state_json:
+            return json.loads(state_json)
+        return {}
+    except Exception as e:
+        print("Error retrieving agent state from Redis:", str(e))
+        return {}
 
 #function to delete agent state from redis based on user_id and key
 def delete_agent_state(user_id:str, key:str):
-    redis.delete(f"user_id:{user_id}:conversation_id:{key}:state")
+    try:
+        redis.delete(f"user_id:{user_id}:conversation_id:{key}:state")
+    except Exception as e:
+        print("Error deleting agent state from Redis:", str(e))
 
 #function to publish and event to a Redis channel for inter-agent communication
 def publish_event(channel:str, event_data:dict):
-    redis.publish(channel, json.dumps(event_data))
+    try:
+        redis.publish(channel, json.dumps(event_data))
+    except Exception as e:
+        print("Error publishing event to Redis channel:", str(e))
