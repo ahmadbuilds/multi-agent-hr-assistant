35f24207a6cd69f7fa396a01f51b1f892ec3c8d0
Ahmad <crisitiano678@gmail.com>
2026-02-08 23:08:11 +0500

    Clerk Tool creation for Ticket Creation completed

--- src/multi-agent-hr-assistant/domain/tools/clerk_tool.py
+++ src/multi-agent-hr-assistant/domain/tools/clerk_tool.py
@@ -1,5 +1,6 @@
 from langchain.tools import tool 
-from domain.ports import LeaveBalancePort
+from domain.ports import LeaveBalancePort, TicketCreationPort
+from domain.entities import TicketCreation
 
 #wrapper function to get leave balance using the LeaveBalancePort
 def make_get_leave_balance_tool(leave_balance_port:LeaveBalancePort):
@@ -16,3 +17,19 @@ def make_get_leave_balance_tool(leave_balance_port:LeaveBalancePort):
         return leave_balance_port.get_leave_balance()
     
     return get_leave_balance
+
+#wrapper function to create the Clerk Agent Get Balance Tool
+def make_ticket_creation_tool(ticket_creation_port:TicketCreationPort,ticket_data:TicketCreation):
+    @tool(
+        name="clerk_create_ticket",
+        description="Use this tool to create a ticket for the current logged in user. Input should include subject and description."
+    )
+    def create_ticket() -> bool:
+        """
+        Tool function to create a ticket for the current logged in user
+        Returns:
+            bool: True if ticket creation is successful, False otherwise
+        """
+        return ticket_creation_port.create_ticket(ticket_data)
+    
+    return create_ticket
