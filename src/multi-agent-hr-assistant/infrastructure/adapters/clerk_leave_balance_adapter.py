aa5be6e1156b993cb5f174cc4144acf4c235e804
Ahmad <crisitiano678@gmail.com>
2026-02-11 18:06:18 +0500

    Clerk Leave balance adapter updated

--- src/multi-agent-hr-assistant/infrastructure/adapters/clerk_leave_balance_adapter.py
+++ src/multi-agent-hr-assistant/infrastructure/adapters/clerk_leave_balance_adapter.py
@@ -5,14 +5,16 @@ from config import CLERK_API_KEY
 class ClerkLeaveBalanceAdapter(LeaveBalancePort):
     def __init__(self):
         self.api_key = CLERK_API_KEY
-    def get_leave_balance(self) -> int:
+    def get_leave_balance(self, token:str) -> int:
         """
         Method to get leave balance for the current logged in user
+        Args:
+            token (str): Authentication token of the user for whom to fetch the leave balance
         Returns:
             int: leave balance in days
         """
         try:
-            response = requests.get(f"{self.api_url}/leave_balance")
+            response = requests.get(f"{self.api_key}/leave_balance", headers={"Authorization": f"Bearer {token}"})
             response.raise_for_status()
             data = response.json()
             return data.get("leave_balance", 0)
