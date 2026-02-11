53de87f5266cd681132c00856688f18ea81ed89d
Ahmad <crisitiano678@gmail.com>
2026-02-11 18:41:45 +0500

    Clerk tool updated for ticket creation

--- src/multi-agent-hr-assistant/infrastructure/adapters/clerk_ticket_creation_adapter.py
+++ src/multi-agent-hr-assistant/infrastructure/adapters/clerk_ticket_creation_adapter.py
@@ -8,18 +8,18 @@ class ClerkTicketCreationAdapter(TicketCreationPort):
     def __init__(self):
         self.clerk_api_key=CLERK_API_KEY
 
-    def create_ticket(self,ticket_data:TicketCreation,user_id:str) -> bool:
+    def create_ticket(self,ticket_data:TicketCreation,token:str) -> bool:
         """
         Method to create a ticket based on the provided ticket data
         Args:
             ticket_data (TicketCreation): data required to create a ticket
+            token (str): Authentication token of the user for whom to create the ticket
         Returns:
             bool: True if ticket creation is successful, False otherwise
         """
         try:
             ticket_data_dict=ticket_data.model_dump()
-            ticket_data_dict["user_id"]=user_id
-            response=requests.post(f"{self.clerk_api_key}/ticket_creation",json=ticket_data_dict)
+            response=requests.post(f"{self.clerk_api_key}/ticket_creation",json=ticket_data_dict,headers={"Authorization": f"Bearer {token}"})
             response.raise_for_status()
             data=response.json()
             return data.get("status",False)
