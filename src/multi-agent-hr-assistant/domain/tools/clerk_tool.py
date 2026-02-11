6d4ecca2612cc9e3c8ad5cb055ccc63c2895c2e4
Ahmad <crisitiano678@gmail.com>
2026-02-11 18:06:53 +0500

    Clerk tool updated

--- src/multi-agent-hr-assistant/domain/tools/clerk_tool.py
+++ src/multi-agent-hr-assistant/domain/tools/clerk_tool.py
@@ -3,7 +3,7 @@ from domain.ports import LeaveBalancePort, TicketCreationPort
 from domain.entities import TicketCreation
 
 #wrapper function to get leave balance using the LeaveBalancePort
-def make_get_leave_balance_tool(leave_balance_port:LeaveBalancePort,user_id:str):
+def make_get_leave_balance_tool(leave_balance_port:LeaveBalancePort,token:str):
     @tool(
         name="get_balance",
         description="Use this tool to get the leave balance of the current logged in user. No input is required."
@@ -14,7 +14,7 @@ def make_get_leave_balance_tool(leave_balance_port:LeaveBalancePort,user_id:str)
         Returns:
             int: leave balance in days
         """
-        return leave_balance_port.get_leave_balance(user_id)
+        return leave_balance_port.get_leave_balance(token)
     
     return get_leave_balance
 
