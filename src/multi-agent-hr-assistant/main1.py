e43d41c279c4e218c6ac8082fc119695b9659752
Ahmad <crisitiano678@gmail.com>
2026-02-13 13:03:28 +0500

    main1 for testing updated

--- src/multi-agent-hr-assistant/main1.py
+++ src/multi-agent-hr-assistant/main1.py
@@ -14,13 +14,15 @@
 from application.agents.clerk import ClerkAgent
 from infrastructure.llm_providers.ollama_provider import create_model_instance
 from infrastructure.adapters.clerk_leave_balance_adapter import ClerkLeaveBalanceAdapter
-
+from infrastructure.adapters.clerk_ticket_creation_adapter import ClerkTicketCreationAdapter
 #Creating LLM Model Instance
 llm_model=create_model_instance()
 #Creating Leave Balance Port Instance
 leave_balance_port=ClerkLeaveBalanceAdapter()
+#Creating Ticket Creation Port Instance
+ticket_creation_port=ClerkTicketCreationAdapter()
 #Creating Clerk Agent Instance
-clerk_agent_instance=ClerkAgent(llm_model,leave_balance_port)
+clerk_agent_instance=ClerkAgent(llm_model,leave_balance_port,ticket_creation_port)
 #Creating Clerk Agent Graph
 clerk_agent_graph=clerk_agent_instance.create_clerk_agent_graph()
 #Displaying the Clerk Agent Graph
