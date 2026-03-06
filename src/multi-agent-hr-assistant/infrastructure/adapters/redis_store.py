30921bf8239173cd173397118856a3983a51cdf1
Ahmad <crisitiano678@gmail.com>
2026-03-06 11:53:37 +0500

    Adapters implementation of document hahs deletion

--- src/multi-agent-hr-assistant/infrastructure/adapters/redis_store.py
+++ src/multi-agent-hr-assistant/infrastructure/adapters/redis_store.py
@@ -1,5 +1,5 @@
 from domain.ports import DocumentStorePort
-from infrastructure.redis.redis_client import save_document_version_name,get_document_version_name,save_document_hash,get_document_hash
+from infrastructure.redis.redis_client import save_document_version,get_document_version,save_document_hash_to_redis,get_document_hash_to_redis,delete_document_hash_to_redis
 from infrastructure.redis.redis_config import get_redis_client
 
 class RedisDocumentStore(DocumentStorePort):
@@ -7,13 +7,16 @@ class RedisDocumentStore(DocumentStorePort):
         self.redis = get_redis_client()
 
     def save_document_version_name(self, updated_name: str) -> bool:
-        return save_document_version_name(updated_name)
+        return save_document_version(updated_name)
 
     def get_document_version_name(self) -> str:
-        return get_document_version_name()
+        return get_document_version()
 
     def save_document_hash(self, document_id: str, document_hash: str) -> bool:
-        return save_document_hash(document_id, document_hash)
+        return save_document_hash_to_redis(document_id, document_hash)
 
     def get_document_hash(self, document_id: str) -> str:
-        return get_document_hash(document_id)
+        return get_document_hash_to_redis(document_id)
+
+    def delete_document_hash(self, document_id: str) -> bool:
+        return delete_document_hash_to_redis(document_id)
