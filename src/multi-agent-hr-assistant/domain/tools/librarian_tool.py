bd011ca6eab597e62ad6ba0d28f8ffcd6d4066bd
Ahmad <crisitiano678@gmail.com>
2026-03-11 11:47:56 +0500

    librarian tool parameter bug fixed

--- src/multi-agent-hr-assistant/domain/tools/librarian_tool.py
+++ src/multi-agent-hr-assistant/domain/tools/librarian_tool.py
@@ -2,7 +2,7 @@ from domain.ports import LibrarianRetrievalPort,LibrarianInsertionPort,Librarian
 from langchain.tools import tool
 
 def make_librarian_retrieval_tool(retrieval_port:LibrarianRetrievalPort):
-    @tool(name="librarian_retrieval_tool", description="Tool for the Librarian Agent to retrieve relevant document based on the user query.")
+    @tool("librarian_retrieval_tool", description="Tool for the Librarian Agent to retrieve relevant document based on the user query.")
     def librarian_retrieval_tool(query:str)->str:
         """
         Tool for the Librarian Agent to retrieve relevant document based on the user query
@@ -15,7 +15,7 @@ def make_librarian_retrieval_tool(retrieval_port:LibrarianRetrievalPort):
     return librarian_retrieval_tool
 
 def make_librarian_update_tool(update_port:LibrarianUpdatePort):
-    @tool(name="librarian_update_tool", description="Tool for the Librarian Agent to update an existing document based on the provided content.")
+    @tool("librarian_update_tool", description="Tool for the Librarian Agent to update an existing document based on the provided content.")
     def librarian_update_tool(document_content:str)->bool:
         """
         Tool for the Librarian Agent to update an existing document based on the provided content
@@ -28,7 +28,7 @@ def make_librarian_update_tool(update_port:LibrarianUpdatePort):
     return librarian_update_tool
 
 def make_librarian_insertion_tool(insertion_port:LibrarianInsertionPort):
-    @tool(name="librarian_insertion_tool", description="Tool for the Librarian Agent to insert a new document based on the provided content.")
+    @tool("librarian_insertion_tool", description="Tool for the Librarian Agent to insert a new document based on the provided content.")
     def librarian_insertion_tool(document_content:str)->bool:
         """
         Tool for the Librarian Agent to insert a new document based on the provided content
--- src/multi-agent-hr-assistant/main1.py
+++ src/multi-agent-hr-assistant/main1.py
@@ -28,11 +28,41 @@ from infrastructure.llm_providers.ollama_provider import create_model_instance
 # #Displaying the Clerk Agent Graph
 # clerk_agent_instance.display_clerk_agent_graph(clerk_agent_graph)
 
-from application.agents.supervisor import SupervisorAgent
-from infrastructure.adapters.supervisor_clerk_graph_executor import SupervisorClerkGraphExecutor
-from application.states import SupervisorState
+# from application.agents.supervisor import SupervisorAgent
+# from infrastructure.adapters.supervisor_clerk_graph_executor import SupervisorClerkGraphExecutor
+# from application.states import SupervisorState
+# llm_model=create_model_instance()
+# SupervisorClerkGraphExecutorPort=SupervisorClerkGraphExecutor(SupervisorState)
+# supervisor_agent_instance=SupervisorAgent(llm_model,SupervisorClerkGraphExecutorPort)
+# supervisor_agent_graph=supervisor_agent_instance.create_supervisor_agent_graph()
+# supervisor_agent_instance.build_supervisor_agent_graph_image(supervisor_agent_graph)
+
+from application.agents.librarian import LibrarianAgent
+from infrastructure.adapters.librarian_retrieval_adapter import LibrarianRetrievalAdapter
+from infrastructure.adapters.librarian_insertion_adapter import LibrarianInsertionAdapter
+from infrastructure.adapters.librarian_updation_adapter import LibrarianUpdateAdapter
+from infrastructure.llm_providers.ollama_provider import create_model_instance
+from infrastructure.adapters.chroma_store import ChromaVectorStore
+from infrastructure.adapters.redis_store import RedisDocumentStore
+from application.services.ingestion import IngestionService
+#Creating LLM Model Instance
 llm_model=create_model_instance()
-SupervisorClerkGraphExecutorPort=SupervisorClerkGraphExecutor(SupervisorState)
-supervisor_agent_instance=SupervisorAgent(llm_model,SupervisorClerkGraphExecutorPort)
-supervisor_agent_graph=supervisor_agent_instance.create_supervisor_agent_graph()
-supervisor_agent_instance.build_supervisor_agent_graph_image(supervisor_agent_graph)
+
+#Creating Redis Document Store Port Instance
+redis_store=RedisDocumentStore()
+#Creating Chroma Vector Store Port Instance
+vector_store=ChromaVectorStore()
+#Creating Ingestion Service Instance
+ingestion_service=IngestionService(redis_store,vector_store)
+
+#Creating Librarian Agent Adapters
+librarian_retrieval_adapter=LibrarianRetrievalAdapter()
+librarian_insertion_adapter=LibrarianInsertionAdapter(ingestion_service)
+librarian_update_adapter=LibrarianUpdateAdapter(ingestion_service)
+
+#Creating Librarian Agent Instance
+librarian_agent_instance=LibrarianAgent(llm_model,librarian_retrieval_adapter,librarian_insertion_adapter,librarian_update_adapter)
+#Creating Librarian Agent Graph
+librarian_agent_graph=librarian_agent_instance.create_librarian_agent_graph()
+#Displaying the Librarian Agent Graph
+librarian_agent_instance.display_librarian_agent_graph(librarian_agent_graph)
