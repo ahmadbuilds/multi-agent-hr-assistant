d92144280d936af6627420b3de1c62c223a260d9
Ahmad <crisitiano678@gmail.com>
2026-03-12 15:29:04 +0500

    Chroma client vector store bugs fixed

--- src/multi-agent-hr-assistant/infrastructure/vector_store/chroma_client.py
+++ src/multi-agent-hr-assistant/infrastructure/vector_store/chroma_client.py
@@ -4,7 +4,7 @@ from langchain_huggingface import HuggingFaceEmbeddings
 #function to create the instance of the vector store
 def create_chroma_instance(persist_dir:str='./data/policies')->Chroma:
     #creating Hugging Face Embedding model
-    embedding_model=HuggingFaceEmbeddings(model="sentence-transformers/all-mpnet-base-v2")
+    embedding_model=HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
 
     vector_store=Chroma(
         collection_name='policy_docs',
