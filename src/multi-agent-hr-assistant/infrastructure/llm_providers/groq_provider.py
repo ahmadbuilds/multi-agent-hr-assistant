fbb6af716c97834db23ea0385e8aa436749ffa75
Ahmad <crisitiano678@gmail.com>
2026-03-14 21:48:24 +0500

    LLm model Changed for better reasoning

--- src/multi-agent-hr-assistant/infrastructure/llm_providers/groq_provider.py
+++ src/multi-agent-hr-assistant/infrastructure/llm_providers/groq_provider.py
@@ -1,6 +1,6 @@
 from langchain_groq import ChatGroq
 
-def create_model_instance(model_name:str="openai/gpt-oss-120b",temperature:float=0,total_retries:int=3):
+def create_model_instance(model_name:str="meta-llama/llama-4-scout-17b-16e-instruct",temperature:float=0,total_retries:int=3):
     model=ChatGroq(
         model=model_name,
         temperature=temperature,
